375a290dd2bbe1ec6b326a37106a49438b6d1cf6
Abod <112323349+aabb00dd@users.noreply.github.com>
2025-03-18 04:31:00 +0100

    Update DiskretSpektrum_modifierad.m

--- FFTanalys/DiskretSpektrum_modifierad.m
+++ FFTanalys/DiskretSpektrum_modifierad.m
@@ -4,79 +4,78 @@ clc
 load signal.mat
 
 
-N=fs*2; %Antal sampel i signalen (obs! skriptet är skriven bara för N med jämna heltal)
-n=0:10*N; %vektor med index
+N=fs*2; % Number of samples in the signal (note! the script is written only for N with even integers)
+n=0:10*N; % vector with indices
 
-%signalen
-A=2; %Amplitud
-k=50; %Koefficient
-fas=0; %Fas angiven i grader
-s=A*cos((2*pi/N)*k*n+fas*(pi/180)); %ren signal
+% the signal
+A=2; % Amplitude
+k=50; % Coefficient
+fas=0; % Phase given in degrees
+s=A*cos((2*pi/N)*k*n+fas*(pi/180)); % pure signal
 figure
 plot(n,s);
-xlabel('Sampel n')
+xlabel('Sample n')
 ylabel('Amplitude')
 title('Pure sine signal')
 
 N1=N;
-%beräkna spektrum
-tic;%starta tidstagarur
-S=abs(fft(s,N1)); %Obs, vi letar efter frekvensen, därav magnituden av spektrat fullt duglig. FFT utförs på hela signalen som är N sampel lång.
-elapsedTime=toc;%Stoppa tidstagarur och visa tiden i sekunder i Workspace samt spara i parameter elapsedTime.
-S=S(1:N1/2+1)/N1; %Reell data ger symetrisk spektrum, räcker halvan. Division med N för att få rätt amplitud
+% calculate spectrum
+tic; % start timer
+S=abs(fft(s,N1)); % Note, we are looking for the frequency, hence the magnitude of the spectrum is fully sufficient. FFT is performed on the entire signal which is N samples long.
+elapsedTime=toc; % Stop timer and display the time in seconds in the Workspace and save in parameter elapsedTime.
+S=S(1:N1/2+1)/N1; % Real data gives symmetric spectrum, half is enough. Division by N to get the correct amplitude
 
-%plotta med koefficientr k på x-axeln
+% plot with coefficient k on the x-axis
 figure
 stem(0:N1/2,S)
-xlabel('Koefficient k')
+xlabel('Coefficient k')
 ylabel('Magnitude')
 title('Spectrum of the observed signal')
 
-%plotta med radianer på x-axeln
+% plot with radians on the x-axis
 figure
 stem((0:N1/2)*(2*pi/N1),S)
 xlabel('Frequency [rad]')
 ylabel('Magnitude')
 title('Spectrum of the observed signal')
 
-%signal1
-A1=5; %Amplitud
-k1=50; %Koefficient
-fas1=0; %Fas angiven i grader
-s1=A1*cos((2*pi/N)*k1*n+fas1*(pi/180)); %ren signal
-%signal2
-A2=3; %Amplitud
-k2=75; %Koefficient
-fas2=45; %Fas angiven i grader
-s2=A2*cos((2*pi/N)*k2*n+fas2*(pi/180)); %ren signal
-%signal3
-A3=2; %Amplitud
-k3=200; %Koefficient
-fas3=90; %Fas angiven i grader
-s3=A3*cos((2*pi/N)*k3*n+fas3*(pi/180)); %ren signal
+% signal1
+A1=5; % Amplitude
+k1=50; % Coefficient
+fas1=0; % Phase given in degrees
+s1=A1*cos((2*pi/N)*k1*n+fas1*(pi/180)); % pure signal
+% signal2
+A2=3; % Amplitude
+k2=75; % Coefficient
+fas2=45; % Phase given in degrees
+s2=A2*cos((2*pi/N)*k2*n+fas2*(pi/180)); % pure signal
+% signal3
+A3=2; % Amplitude
+k3=200; % Coefficient
+fas3=90; % Phase given in degrees
+s3=A3*cos((2*pi/N)*k3*n+fas3*(pi/180)); % pure signal
 
-s=s1+s2+s3; %signal s innehåller nu 3st frekvens komponenter med varsin amplitud, frekvens och fas.
+s=s1+s2+s3; % signal s now contains 3 frequency components with their own amplitude, frequency, and phase.
 figure
 plot(n,s);
-xlabel('Sampel')
+xlabel('Sample')
 ylabel('Amplitude')
 title('Signal with multiple components')
 
-%beräkna spektrum
-S=abs(fft(signal,N1)); %Obs, vi letar efter frekvensen, därav magnituden av spektrat fullt duglig
-S=S(1:N1/2+1)/N1; %Reell data ger symetrisk spektrum, räcker halvan. Division med N för att få rätt amplitud
+% calculate spectrum
+S=abs(fft(signal,N1)); % Note, we are looking for the frequency, hence the magnitude of the spectrum is fully sufficient
+S=S(1:N1/2+1)/N1; % Real data gives symmetric spectrum, half is enough. Division by N to get the correct amplitude
 
-%plotta med koefficientr k på x-axeln
+% plot with coefficient k on the x-axis
 figure
 stem((0:N1/2),S)
-xlabel('Koefficient k')
+xlabel('Coefficient k')
 ylabel('Magnitude')
 title('Spectrum of the observed signal')
 
-%plotta med radianer på x-axeln
+% plot with radians on the x-axis
 figure
 stem((0:N1/2)*(2*pi/N1),S)
 xlabel('Frequency [rad]')
 ylabel('Magnitude')
 title('Spectrum of the observed signal')
-
