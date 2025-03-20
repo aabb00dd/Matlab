54efb9b8bfb4321562bd0ccc62423240a1b97238
Abod <112323349+aabb00dd@users.noreply.github.com>
2025-03-20 03:00:05 +0100

    Update SquareSynthesis.m

--- FourierSerier/SquareSynthesis.m
+++ FourierSerier/SquareSynthesis.m
@@ -1,33 +1,33 @@
-%Skriptet demosntrerar hur man syntetiserar en periodisk signal med den
-%?ndliga Fourier Serier Syntesen med N ak koefficienter. 
+% The script demonstrates how to synthesize a periodic signal using
+% the finite Fourier Series Synthesis with N ak coefficients.
 close all
 clear
 clc
 
-N=379; %Antal koefficienter i syntesen 
-T0=4; %Periodtiden f?r signalen
-t=0:0.01:3*T0; %tidsvektorn
+N = 379; % Number of coefficients in the synthesis
+T0 = 4; % Period of the signal
+t = 0:0.01:3*T0; % Time vector
 
-%h?r skapas original signal i detta fall en ren fyrkantsv?g
-ph=(length(t)-1)/6;
-original=[ones(1,ph/2) zeros(1,ph) ones(1,ph) zeros(1,ph) ones(1,ph) zeros(1,ph) ones(1,ph/2) 1];
+% Here, the original signal is created, in this case, a pure square wave
+ph = (length(t) - 1) / 6;
+original = [ones(1, ph/2) zeros(1, ph) ones(1, ph) zeros(1, ph) ones(1, ph) zeros(1, ph) ones(1, ph/2) 1];
 
-%Syntes for-loopen med plott f?r varje tillagd komponent ak
-%fyrkantsv?gen har 0 i j?mna k komponenter.
-xN=0.5*ones(size(t)); %start p? signalen xN(t) med f?rsta koefficienten a0
-for k=1:2:N
-    %formeln f?r ak tagen fr?n Exempel C-1 i boken
-    %skriven p? formeln enligt exercise C.3 (reell signal)
-    ak=sin(pi*k/2)/(pi*k);
-    xN=xN+2*ak*cos(2*pi/T0*k*t); %addition av ak
+% Synthesis loop with plotting for each added component ak
+% The square wave has 0 in even k components.
+xN = 0.5 * ones(size(t)); % Start of the signal xN(t) with the first coefficient a0
+for k = 1:2:N
+    % Formula for ak taken from Example C-1 in the book
+    % Written according to the formula in exercise C.3 (real signal)
+    ak = sin(pi * k / 2) / (pi * k);
+    xN = xN + 2 * ak * cos(2 * pi / T0 * k * t); % Addition of ak
     
-    %plottning av den syntetiserad signal ?ver den ursprungliga signalen
-    plot(t,original,'k')
+    % Plotting the synthesized signal over the original signal
+    plot(t, original, 'k')
     hold on
-    plot(t,xN)
+    plot(t, xN)
     axis([0 12 -0.2 1.2])
     title(num2str(k))
     hold off
-    %pausa med 0.25s
+    % Pause for 0.25 seconds
     pause(0.25)
 end
