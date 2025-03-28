fe79c54597b5f2107c53755a4f0c4d83e841c3d2
Abod <112323349+aabb00dd@users.noreply.github.com>
2025-03-28 01:02:43 +0100

    Update helvagslikriktad_sinusvag_coeff.m

--- FourierSerier/helvagslikriktad_sinusvag_coeff.m
+++ FourierSerier/helvagslikriktad_sinusvag_coeff.m
@@ -1,34 +1,33 @@
-%Skriptet demostrerar hur man plottar koefficienterna ak fr?n
-%Fourier Serier Analysen med hj?lp av stem().
-%I detta fall g?ller koefficienterna f?r en fyrkantsv?g
-%OBS! Bara origo och den h?gra sidan av spektrummet ber?knas och plottas.
+% The script demonstrates how to plot the coefficients ak from
+% Fourier Series Analysis using stem().
+% In this case, the coefficients apply to a square wave.
+% NOTE! Only the origin and the right side of the spectrum are calculated and plotted.
 close all
 clear
 clc
 
-N=16; %Val av antal Koefficienter vi vill plotta
-T0=4; %periodtiden f?r den periodiska signalen
+N=16; % Number of coefficients to plot
+T0=4; % Period of the periodic signal
 
 ak=zeros(1,N);
 for k=1:N
-    ak(k)=2 / (pi * (1 - 4 * k^2)); %formeln f?r ak tagen fr?n Exempel 3.34 i boken
+    ak(k)=2 / (pi * (1 - 4 * k^2)); % Formula for ak from Example 3.34 in the book
 end
-ak=[2/pi ak]; %l?gg till a0
+ak=[2/pi ak]; % Add a0
 
-%H?r plottas koefficienterna med tecknet bevarad
+% Plot coefficients with sign preserved
 stem(0:N,ak)
 xlabel('Coeff k');
 ylabel('Amplitude');
 
-%H?r plottas absolutbellopet f?r koefficienterna
+% Plot absolute values of coefficients
 figure
 stem(0:N,abs(ak))
 xlabel('Coeff k');
 ylabel('Magnitude');
 
-%H?r plottas absolutbellopet, samt omr?knad k ordningen till frekvens
+% Plot absolute values with k converted to frequency
 figure
 stem((0:N)*(1/T0),abs(ak))
 xlabel('Freq. [Hz]');
 ylabel('Magnitude');
-
