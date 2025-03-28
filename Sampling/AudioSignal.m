9fe8d6dd7b2b47c25f3a574d92a7a90a8af1f671
Abod <112323349+aabb00dd@users.noreply.github.com>
2025-03-28 01:35:23 +0100

    Update AudioSignal.m

--- Sampling/AudioSignal.m
+++ Sampling/AudioSignal.m
@@ -1,48 +1,47 @@
-%Demo av hur man spelar in egen audiodata med datorns mikrofoner direkt in
-%i MATLAB. B??r funka f??r b??de PC och Mac.
+% Demo of how to record audio data using computer's microphone directly into MATLAB
+% Should work for both PC and Mac
 clear
 close all
 clc
 
-%Parametrar
-Fs=200000;%Samplingsfrekvensen i Hz (1000 till 200000 i Josefs dator)
-nBits=24;  %Antal bitar som g??r att v??lja 8, 16 och 24
-nChannels=2; %Antal kanaler, en kanal per mikrofon
-RecTime=10; %Inspelningstid i sec
+% Parameters
+Fs = 200000; % Sampling frequency in Hz (1000 to 200000 in Josef's computer)
+nBits = 24;  % Number of bits to select (8, 16, and 24)
+nChannels = 2; % Number of channels, one channel per microphone
+RecTime = 10; % Recording time in seconds
 
-myVoice = audiorecorder(Fs,nBits,nChannels);%
+myVoice = audiorecorder(Fs, nBits, nChannels);
 
-%Skapar utskrift i Command Window f??r att markera n??r inspelning startar
-%och slutar.
+% Create output in Command Window to mark when recording starts and stops
 myVoice.StartFcn = 'disp(''Start speaking'')';
 myVoice.StopFcn = 'disp(''End of recording'')';
 
-record(myVoice, RecTime);%startar inspelning av audio
+record(myVoice, RecTime); % Start audio recording
 
-pause(RecTime+1);%Pausar tills inspelning ??r f??rdig (n??dv??ndig)
-%play(myVoice);%Spelar upp den inspelat data
+pause(RecTime+1); % Pause until recording is complete (necessary)
+% play(myVoice); % Play back the recorded data
 
-%Extrahera inslepad data ur objektet myVoice
-data = getaudiodata(myVoice);%vid val av fler kanaler f??r vi en matris 
-audio=data(:,1);%Ta data f??r f??rsta kanalen
+% Extract recorded data from the myVoice object
+data = getaudiodata(myVoice); % When selecting multiple channels we get a matrix
+audio = data(:,1); % Take data from first channel
 
-%sound() och soundsc() ??r tv?? andra funktioner som kan anv??ndas f??r att
-%spela upp ljud. Om n??gon av dessa ska anv??ndas kommentera bort play() p??
-%rad 23. Det g??r att v??lja andra alternativ f??r Fs och nBits f??r att
-%manipulera inspelningen.
+% sound() and soundsc() are two other functions that can be used to
+% play audio. If you want to use either of these, comment out play()
+% on line 23. You can choose other options for Fs and nBits to
+% manipulate the recording.
 
-%sound(audio,Fs,nBits);
+% sound(audio,Fs,nBits);
 soundsc(audio,Fs,nBits);
 
-t=0:1/Fs:RecTime-1/Fs;%Tidsvektor
-%plottar audio tidssignalen
+t = 0:1/Fs:RecTime-1/Fs; % Time vector
+% Plot audio time signal
 figure(1)
 plot(t,audio);
 xlabel('Time (secs)');
 ylabel('Amplitude')
 
-%plottar frekvensinneh??llet (kr??ver Signal Processing Toolbox)
-%mer om detta senare i kursen n??r vi behandlar kapitel 8
-%h??r ??r den med bara f??r att ge lite inblick i frekvensinneh??llet
+% Plot frequency content (requires Signal Processing Toolbox)
+% More on this later in the course when we cover chapter 8
+% Included here just to provide some insight into the frequency content
 figure(2)
 spectrogram(audio,'yaxis')
