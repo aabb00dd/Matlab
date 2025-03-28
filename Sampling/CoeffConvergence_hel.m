41a06ced57122deec2f7a2c2f86888dc4e349ea1
Abod <112323349+aabb00dd@users.noreply.github.com>
2025-03-28 01:41:26 +0100

    Update CoeffConvergence_hel.m

--- Sampling/CoeffConvergence_hel.m
+++ Sampling/CoeffConvergence_hel.m
@@ -1,26 +1,26 @@
-%Skriptet visar hur man estimerar ak parametrarna f?r en fyrkantsv?g
-%med den Diskreta Fourier Serien, d? vi bara har tillg?ng till en samplad
-%periodisk signal f[n]
+% The script shows how to estimate the ak parameters for a square wave
+% using the Discrete Fourier Series, when we only have access to a sampled
+% periodic signal f[n]
 close all
 clear all
 clc
 
-k=2; %v?lj koefficient ak
-Nmax=200; %Maximal antal sampel under signalens ena period
-ak=[]; %tom vektor f?r att spara olika estimat med 
+k = 2; % select coefficient ak
+Nmax = 200; % maximum number of samples during one period of the signal
+ak = []; % empty vector to store different estimates
 
-for N=1:1:Nmax %for-loop f?r att ?ka antal sampel N
+for N = 1:1:Nmax % for-loop to increase number of samples N
     t = linspace(0, pi, N);
     f = sin(t);
-    L = length(f)
-    akN=0;%nollst?ll parametern
-    for n=0:L-1
-        akN=akN+f(n+1)*exp(-j*((2*pi)/N)*k*n);
+    L = length(f);
+    akN = 0; % reset parameter
+    for n = 0:L-1
+        akN = akN + f(n+1)*exp(-j*((2*pi)/N)*k*n);
     end
-    ak(end+1)=akN/N;%spara estimatet (inte den mest effektivaste s?ttet)
+    ak(end+1) = akN/N; % save estimate (not the most efficient way)
 end
 
-theoretical_value = (2 / (pi * (1 - 4 * k^2)))*ones(1,length(1:1:Nmax))
+theoretical_value = (2 / (pi * (1 - 4 * k^2)))*ones(1,length(1:1:Nmax));
 
 plot(1:1:Nmax, theoretical_value, 'r');
 hold on;
