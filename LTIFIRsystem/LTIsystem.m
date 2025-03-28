272f6f5c85efe9f69bb67927834a305934bf7ca3
Abod <112323349+aabb00dd@users.noreply.github.com>
2025-03-28 01:27:28 +0100

    Update LTIsystem.m

--- LTIFIRsystem/LTIsystem.m
+++ LTIFIRsystem/LTIsystem.m
@@ -2,54 +2,54 @@ close all
 clear
 clc
 
-b=[2 -(pi - 1) 2]; %bk koefficienter för LTI FIR system
+b = [2 -(pi - 1) 2]; % bk coefficients for LTI FIR system
 
-N=512;%Antal sampel, välj jämn nummer
-omega=linspace(-pi,pi,N);%skapa vektor med samplad frekvensaxeln mellan -pi och pi med N antal sampel
-H=freqz(b,1,omega);%räknar fram frekvenssvaret H för FIR filtret (komplex)
-absH=abs(H);%Magnituden för FIR filtret
-argH=angle(H);%Fasen för FIR filtret i radianer
+N = 512; % Number of samples, choose even number
+omega = linspace(-pi, pi, N); % Create vector with sampled frequency axis between -pi and pi with N samples
+H = freqz(b, 1, omega); % Calculate frequency response H for FIR filter (complex)
+absH = abs(H); % Magnitude of FIR filter
+argH = angle(H); % Phase of FIR filter in radians
 
-%Plotta Magnituden
+% Plot Magnitude
 subplot(2,1,1)
-plot(omega,absH)
+plot(omega, absH)
 axis([-pi pi 0 max(absH)])
 xlabel('Frequency [rad]')
 ylabel('Magnitude')
 
-%Plotta Fasen
+% Plot Phase
 subplot(2,1,2)
-plot(omega,argH)
+plot(omega, argH)
 axis([-pi pi -pi pi])
 xlabel('Frequency [rad]')
 ylabel('Phase [rad]')
 
-%Testsignal
-n=0:N-1;%index vektorn
-A1=2; %Amplitud för komponenr 1
-A2=2; %Amplitud för komponent 2
-l1=300;%sampelposition komponent 1
-l2=300;%sampelposition komponent 2
-w1=0.66;%välj frekvensen för testsignalen ur den samplad omega vektorn (för N=512, börjar positiva omega från sampel 257)
-w2=1;%frekvcens komponent 2, sammanfaller med ett nollställe
-fas1=0; %Fas för komponent 1 i grader
-fas2=0; %Fas för komponent 2 i grader
-s1=A1*cos(w1*n+fas1*(pi/180)); %Komponent 1
-s2=A2*cos(w2*n+fas2*(pi/180)); %Komponent 2
-s=s1+s2;
-y=filter(b,1,s);
+% Test signal
+n = 0:N-1; % Index vector
+A1 = 2; % Amplitude for component 1
+A2 = 2; % Amplitude for component 2
+l1 = 300; % Sample position component 1
+l2 = 300; % Sample position component 2
+w1 = 0.66; % Select test signal frequency from sampled omega vector (for N=512, positive omega starts from sample 257)
+w2 = 1; % Frequency component 2, coincides with a zero
+phase1 = 0; % Phase for component 1 in degrees
+phase2 = 0; % Phase for component 2 in degrees
+s1 = A1*cos(w1*n + phase1*(pi/180)); % Component 1
+s2 = A2*cos(w2*n + phase2*(pi/180)); % Component 2
+s = s1 + s2;
+y = filter(b, 1, s);
 
-y=conv(s,b,'full');%faltnin av signalen med systemet
+y = conv(s, b, 'full'); % Convolution of signal with system
 figure
-plot(n,y(1:end-length(b)+1))
+plot(n, y(1:end-length(b)+1))
 hold on
 
-%Framräknad med ekv. 6.6 i kursboken kap.6-2
-y2=absH(l1)*A1*cos(w1*n+fas1*(pi/180)+argH(l1));
-y3=absH(l2)*A2*cos(w2*n+fas2*(pi/180)+argH(l2));
-y_total=y2+y3;
-plot(n,y_total,'--k')
+% Pre-calculated using eq. 6.6 in course book chapter 6-2
+y2 = absH(l1)*A1*cos(w1*n + phase1*(pi/180) + argH(l1));
+y3 = absH(l2)*A2*cos(w2*n + phase2*(pi/180) + argH(l2));
+y_total = y2 + y3;
+plot(n, y_total, '--k')
 hold off
 xlabel('Time [s]')
 ylabel('Amplitude')
-legend('Convolved','Calculated')
+legend('Convolved', 'Calculated')
