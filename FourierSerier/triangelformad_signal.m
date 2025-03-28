99685a907767d1c58a96a14aba93245dda483bd7
Abod <112323349+aabb00dd@users.noreply.github.com>
2025-03-28 01:05:07 +0100

    Update triangelformad_signal.m

--- FourierSerier/triangelformad_signal.m
+++ FourierSerier/triangelformad_signal.m
@@ -2,9 +2,9 @@ close all
 clear
 clc
 
-N = 53; % Antal koefficienter i syntesen
-T0 = 4; % Periodtiden f?r signalen
-t = 0:0.01:3*T0; % Tidsvektorn
+N = 53; % Number of coefficients in the synthesis
+T0 = 4; % Period of the signal
+t = 0:0.01:3*T0; % Time vector
 ph = (length(t)-1)/6;
 
 original = abs(sawtooth(pi/T0*t + pi/2, 0.5));
@@ -13,9 +13,9 @@ xN = 0.5;
 for l = 1:1:(N+1)/2
     k = 2*l-1;
     ak = -4/((k^2)*pi^2);
-    xN = xN + ak*cos(2*pi*(1/T0)*k*t); %formeln f?r ak tagen fr?n Exempel C.21 i boken
+    xN = xN + ak*cos(2*pi*(1/T0)*k*t); % Formula for ak from Example C.21 in the book
     
-    %plottning av den syntetiserad signal ?ver den ursprungliga signalen
+    % Plotting the synthesized signal over the original signal
     plot(t,original,'k')
     hold on
     plot(t,xN)
